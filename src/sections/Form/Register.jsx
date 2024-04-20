df7e104d0839f98e5422a4621c44ca338e00bace
shoaibRamzan42 <adnanrmzan420@gmail.com>
2024-04-20 16:10:45 +0500

    upadted

--- src/sections/Form/Register.jsx
+++ src/sections/Form/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
     <Navbar/>
     
     <div className='bg-[#080513]  max-w-[100vw] overflow-x-hidden font-[Urbanist] pt-[130px] md:pt-[150px] overflow-y-scroll  h-screen mx-auto px-4 '>
-      <h1 className='text-center font-extrabold  text-[32px] sm:text-[42px] md:text-[52px] text-green-600 mt-3 '>Welcome to the kYC Registeration centre</h1>
+      <h1 className='text-center font-extrabold  text-[32px] sm:text-[42px] md:text-[52px] text-green-600 mt-3 '>Welcome to the kYC  centre</h1>
       <motion.div
             initial={{ y: "50%", opacity: 0 }}
             // viewport={{ once :true,}}
@@ -18,8 +18,8 @@ const Register = () => {
             whileInView={{ y: 0, opacity: 1 }}
             className=""
           >
-      <div className='gradient-border mb-5   p-3 w-[28rem] mx-auto rounded-md mt-5'>
-      <h1 className='text-[36px]  gradient-text font-semibold text-center'>Registeration Form</h1>
+      <form className='gradient-border mb-5 w-auto   p-3 md:w-[28rem] mx-auto rounded-md mt-5'>
+      <h1 className='text-[36px]  gradient-text font-extrabold text-center'>KYC Form</h1>
       <h1 className='text-white text-[20px] mt-4'>First Name</h1>
       <input required type="text" placeholder='First Name*' className=' m-2 p-3 bg-[#4949497d] text-white  w-[95%] rounded-md' />
       <h1 className='text-white text-[20px] mt-4'>Last Name</h1>
@@ -28,8 +28,8 @@ const Register = () => {
       <input required type="email" placeholder='abc@gmail.com' className=' ml-2 p-3 bg-[#4949497d] text-white  w-[95%] rounded-md' />
       <h1 className='text-white text-[20px] mt-4'>Your Identity No</h1>
       <input required type="number" placeholder='xxxxx-xxxxx-x' className=' ml-2 p-3 bg-[#4949497d] text-white  w-[95%] rounded-md' />
-       <Link to="/app"> <button className='btn-bg p-2 text-white text-center rounded-md active:shadow-lg active:shadow-green-800 hover:scale-105 duration-700 justify-center flex mt-4 w-[120px] mx-auto'>Submit</button></Link>
-      </div>
+       <Link to="/app"> <button type='submit' className='btn-bg p-2 text-white text-center rounded-md active:shadow-lg active:shadow-green-800 hover:scale-105 duration-700 justify-center flex mt-4 w-[120px] mx-auto'>Submit</button></Link>
+      </form>
       </motion.div>
     </div>
     </>
